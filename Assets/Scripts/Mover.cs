c0051face52fa406980e656845cb49c700f13524
therko <pderko@gmail.com>
2018-08-02 22:37:25 +0200

    first commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    public float accelerationFactor;
+
+    void Start ()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(GetComponent<Rigidbody>().velocity * speed * accelerationFactor);
+
+        //GetComponent<Rigidbody>().velocity = transform.forward * speed * Mathf.Exp(GetComponent<Rigidbody>().velocity.z);
+    }
+
+}
